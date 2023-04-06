f0c102b00e9da96c2dacae466abdfa0c68b16582
GuruAshwin <121659162+GuruAshwin@users.noreply.github.com>
2023-04-06 21:04:28 +0530

    Create Sort012.java

---
+++ Sort012.java
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial template for Java
+
+import java.io.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+//User function template for Java
+
+class Solution
+{
+    public static void sort012(int a[], int n)
+    {
+        int zero=0;
+        int one=0;
+        int two=0;
+        for(int i=0;i<n;i++){
+            if(a[i]==0){
+                zero+=1;
+            }
+            else if(a[i]==1){
+                one+=1;
+            }
+            else{
+                two+=1;
+            }
+        }
+            
+        for(int i=0;i<n;i++){
+            if(i<zero){
+                a[i]=0;
+            }
+            else if(i<one+zero){
+                a[i]=1;
+            }
+             else if(i<n){
+                a[i]=2;
+            }
+        }
+    }
+}
+
+//{ Driver Code Starts.
+
+class GFG {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim()); //Inputting the testcases
+        while(t-->0){
+            int n = Integer.parseInt(br.readLine().trim());
+            int arr[] = new int[n];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for(int i=0; i<n; i++){
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+            Solution ob=new Solution();
+            ob.sort012(arr, n);
+            StringBuffer str = new StringBuffer();
+            for(int i=0; i<n; i++){
+                str.append(arr[i]+" ");
+            }
+            System.out.println(str);
+        }
+    }
+}
+
+
+// } Driver Code Ends
